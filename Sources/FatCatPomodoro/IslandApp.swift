12004298dc0a58e90c90acb1f07f5c53ab2a1c99
goosey76 <s0586881@htw-berlin.de>
2026-08-28 10:51:45 +0200

    Fix default placement of BreakTimerOverlay

--- Sources/FatCatPomodoro/IslandApp.swift
+++ Sources/FatCatPomodoro/IslandApp.swift
@@ -287,24 +287,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 class DraggableOverlayWindow: NSWindow {
     override var canBecomeKey: Bool { true }
     
-    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
-        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
-        
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(windowDidMove(_:)),
-            name: NSWindow.didMoveNotification,
-            object: self
-        )
-    }
-    
-    @objc private func windowDidMove(_ notification: Notification) {
+    override func mouseUp(with event: NSEvent) {
+        super.mouseUp(with: event)
         UserDefaults.standard.set(frame.origin.x, forKey: "pomodoro.breakOverlayX")
         UserDefaults.standard.set(frame.origin.y, forKey: "pomodoro.breakOverlayY")
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
     }
 }
 
